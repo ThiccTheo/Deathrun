3d9d8d9e81a3212c2c6632c4bf020ecc0b3b17e3
ThiccTheo <th3o1237@gmail.com>
2022-07-17 03:06:50 -0700

    logging class complete

--- Tools/Logger/Logger.cpp
+++ Tools/Logger/Logger.cpp
@@ -1,34 +1,39 @@
-#include <iostream>
+#include <filesystem>
 
 #include "Logger.hpp"
 
-void Logger::print(const LogLevel logLevel, std::string message)
+void Logger::print(const LogLevel logLevel, std::string message, const std::source_location source)
 {
 	switch (logLevel)
 	{
 		case LogLevel::debug:
-			message.insert(0, "[DEBUG]: ");
-			message.push_back('\n');
+			message.insert(0, "[Debug] ");
+			message.push_back(' ');
+			message += std::format("[{}:{}:{}]\n", (strrchr(source.file_name(), '\\') ? strrchr(source.file_name(), '\\') + 1 : source.file_name()), source.line(), source.column());
 			std::cout << message;
 			break;
 		case LogLevel::info:
-			message.insert(0, "[INFO]: ");
-			message.push_back('\n');
+			message.insert(0, "[Info] ");
+			message.push_back(' ');
+			message += std::format("[{}:{}:{}]\n", (strrchr(source.file_name(), '\\') ? strrchr(source.file_name(), '\\') + 1 : source.file_name()), source.line(), source.column());
 			std::cout << message;
 			break;
 		case LogLevel::warning:
-			message.insert(0, "[WARNING]: ");
-			message.push_back('\n');
+			message.insert(0, "[Warning] ");
+			message.push_back(' ');
+			message += std::format("[{}:{}:{}]\n", (strrchr(source.file_name(), '\\') ? strrchr(source.file_name(), '\\') + 1 : source.file_name()), source.line(), source.column());
 			std::cerr << message;
 			break;
 		case LogLevel::error:
-			message.insert(0, "[ERROR]: ");
-			message.push_back('\n');
+			message.insert(0, "[Error] ");
+			message.push_back(' ');
+			message += std::format("[{}:{}:{}]\n", (strrchr(source.file_name(), '\\') ? strrchr(source.file_name(), '\\') + 1 : source.file_name()), source.line(), source.column());
 			std::cerr << message;
 			break;
 		case LogLevel::critical:
-			message.insert(0, "[CRITICAL]: ");
-			message.push_back('\n');
+			message.insert(0, "[Critical] ");
+			message.push_back(' ');
+			message += std::format("[{}:{}:{}]\n", (strrchr(source.file_name(), '\\') ? strrchr(source.file_name(), '\\') + 1 : source.file_name()), source.line(), source.column());
 			std::cerr << message;
 			break;
 	}
--- Tools/Logger/Logger.hpp
+++ Tools/Logger/Logger.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <iostream>
 #include <format>
+#include <source_location>
 
 enum class LogLevel { debug, info, warning, error, critical, count };
 
@@ -8,34 +10,34 @@ class Logger
 {
 public:
 	template<typename... T>
-	static inline void debug(std::string_view fmt, const T&... t)
+	static inline void debug(std::string_view fmt, const T&... t, const std::source_location source = std::source_location::current())
 	{
-		print(LogLevel::debug, std::vformat(fmt, std::make_format_args(t...)));
+		print(LogLevel::debug, std::vformat(fmt, std::make_format_args(t...)), source);
 	}
 
 	template<typename... T>
-	static inline void info(std::string_view fmt, const T&... t)
+	static inline void info(std::string_view fmt, const T&... t, const std::source_location source = std::source_location::current())
 	{
-		print(LogLevel::info, std::vformat(fmt, std::make_format_args(t...)));
+		print(LogLevel::info, std::vformat(fmt, std::make_format_args(t...)), source);
 	}
 
 	template<typename... T>
-	static inline void warning(std::string_view fmt, const T&... t)
+	static inline void warning(std::string_view fmt, const T&... t, const std::source_location source = std::source_location::current())
 	{
-		print(LogLevel::warning, std::vformat(fmt, std::make_format_args(t...)));
+		print(LogLevel::warning, std::vformat(fmt, std::make_format_args(t...)), source);
 	}
 
 	template<typename... T>
-	static inline void error(std::string_view fmt, const T&... t)
+	static inline void error(std::string_view fmt, const T&... t, const std::source_location source = std::source_location::current())
 	{
-		print(LogLevel::error, std::vformat(fmt, std::make_format_args(t...)));
+		print(LogLevel::error, std::vformat(fmt, std::make_format_args(t...)), source);
 	}
 
 	template<typename... T>
-	static inline void critical(std::string_view fmt, const T&... t)
+	static inline void critical(std::string_view fmt, const T&... t, const std::source_location source = std::source_location::current())
 	{
-		print(LogLevel::critical, std::vformat(fmt, std::make_format_args(t...)));
+		print(LogLevel::critical, std::vformat(fmt, std::make_format_args(t...)), source);
 	}
 private:
-	static void print(const LogLevel logLevel, std::string message);
+	static void print(const LogLevel logLevel, std::string message, const std::source_location source);
 };
